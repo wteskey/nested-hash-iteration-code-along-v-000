906d82f206aaf98514fa90a1c2b50b6a2ec3aa69
William J Teskey <willjteskey@gmail.com>
2018-04-17 22:48:20 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+ contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|  # data on first iteration is => ["chocolate", "vanilla"]
+      if attribute == :favorite_ice_cream_flavors  # data on second iteration is => ["strawberry", "cookie dough", "mint chip"]
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}  # data on second iteration now shows => ["cookie dough", "mint chip"]
+        #binding.pry
+      end
+    end
+  end
 end
+
